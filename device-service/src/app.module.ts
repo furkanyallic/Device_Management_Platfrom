79640ed6e811957185f6d06b29f6dad92c8507b7
furkanyallic <yallicfurkann@gmail.com>
2026-08-14 10:09:28 +0300

    fix:remove database credentials

--- device-service/src/app.module.ts
+++ device-service/src/app.module.ts
@@ -8,20 +8,20 @@ import { DevicesModule } from './modules/devices/devices.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-   TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-    type: 'postgres',
-    host: configService.get<string>('DB_HOST', 'localhost'),
-    port: configService.get<number>('DB_PORT', 5435), // device_db portu 5435 olmalı
-    username: configService.get<string>('DB_USERNAME', 'postgres'),
-    password: configService.get<string>('DB_PASSWORD', '2129374'),
-    database: configService.get<string>('DB_NAME', 'device_db'),
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
-  }),
-}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5435), // device_db portu 5435 olmalı
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
     DevicesModule,
   ],
 })
